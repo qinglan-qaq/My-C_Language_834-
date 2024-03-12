c4fc27d4c757f7d7ef55285bbcb6c71a90c6228d
qinglan-qaq <1710202731@qq.com>
2024-03-12 23:31:41 +0800

    线性表第一次提交

--- Linear_List.c
+++ Linear_List.c
@@ -1,10 +1,41 @@
 #include "stdio.h"
+#include <stdbool.h>
+#define MaxSize 100
+#define ElemType int
 
-void fun() {
+//顺序表
+typedef struct {
+    ElemType data[MaxSize];
+    int length;
+}SqList ;
 
-}
+//初始化
+void InitList(SqList L){
+    L.length = 0;
+};
+//判空
+bool Empty(SqList L){
+    if (L.length == 0){
+        return 0;
+    }else{
+        return 1;
+    }
+};
+//插入
+bool ListInsert(SqList &L, int i, int e){
 
+};
+bool ListDelete(SqList &L, int i, int &e);          //删除
+int GetElem(SqList L, int i);                       //按位查找
+int LocateElem(SqList L, int e);                    //按值查找
+bool LocateChangeElem(SqList &L, int e, int em);    //按值修改
+bool getChangeElem(SqList &L, int i, int em);       //按位修改
+
+void PrintSqList(SqList L);                         //打印函数
 int main() {
-    printf("Linear List");
+
+    SqList L;
+
+
     return 0;
 }
